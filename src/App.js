c209634f476bcedbde25d6d9cf5ccffafc80df24
HammyPig <james.bruce.walden@gmail.com>
2025-09-17 16:07:27 +1000

    feat: end game when player win condition is met

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ export default function Board() {
   const [playerTurn, setPlayerTurn] = useState(0);
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || getWinner(squares)) {
       return;
     }
 
     const nextSquares = squares.slice();
     nextSquares[i] = players[playerTurn];
     setSquares(nextSquares);
-    
+
     setPlayerTurn((playerTurn + 1) % players.length);
   }
 
@@ -45,4 +45,27 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function getWinner(squares) {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
